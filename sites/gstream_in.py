81822c8c42a677318d8038a738a16a31a626c2c4
Lynx187 <Lynx187@users.noreply.github.com>
2014-09-29 17:37:15 +0200

    Update gstream_in.py

--- sites/gstream_in.py
+++ sites/gstream_in.py
@@ -5,7 +5,7 @@ from resources.lib.gui.guiElement import cGuiElement
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.lib.config import cConfig
-import resources.lib.logger
+from resources.lib.logger import *
 from resources.lib.handler.ParameterHandler import ParameterHandler
 
 SITE_IDENTIFIER = 'gstream_in'
